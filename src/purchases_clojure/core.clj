966db04c5c07c645db0d2b9f54d4684fcdee53d6
Paige Hetherington <herbmama@gmail.com>
2016-03-22 18:06:41 -0400

    Parse csv

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,5 +1,31 @@
 (ns purchases-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk]
+            [clojure.pprint :as pp]) ; to print nicely, not as a string line
   (:gen-class))
 
+(def categories (atom []))
+
 (defn -main []
-  (println "Hello, World!"))
+  (let [purchases (slurp "purchases.csv")
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                          (str/split line #","))
+                       purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                          (apply hash-map (interleave header line)))
+                       purchases)
+        purchases (walk/keywordize-keys purchases)
+   
+        _ (println "Please type a category and hit enter.") ; to print within let stmt
+        text (read-line)
+        purchases (filter (fn [line]
+                            (= (:category line) text))
+                          purchases)] 
+    (spit "filtered_purchases.edn" (with-out-str (pp/pprint purchases))) ; replace pr-string to print nicely
+    purchases))
+
+
+  
